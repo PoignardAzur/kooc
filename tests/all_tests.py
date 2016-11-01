fe056674a0e37fc0f65b223e43fddc4b3e4cea97
Olivier FAURE <olivier.faure@epitech.eu>
2016-11-01 13:41:54 +0100

    Rework src and tests to make testing easier
    Update standards file
    Add kooc script
    Rename main.py to __main__.py (so ./src can be run as a module)

--- tests/all_tests.py
+++
@@ -1,6 +0,0 @@
-from . import main
-
-import unittest
-
-if __name__ == '__main__':
-    main.TestMain().runTest()
---
+++ tests/test_src_main.py
@@ -0,0 +1,15 @@
+from src import __main__
+
+import unittest
+
+class TestMain(unittest.TestCase):
+
+    def test_no_file(self):
+        self.assertFalse(__main__.main())
+        self.assertFalse(__main__.main([]))
+        argv = ["Whatever"]
+        self.assertFalse(__main__.main(argv))
+
+    def test_simple_file(self):
+        argv = ["test", "tests/test.kc"]
+        self.assertTrue(__main__.main(argv))
