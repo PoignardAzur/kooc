1ee4fd4d74624e41ec229e7d72923b7541a7b11f
Olivier FAURE <olivier.faure@epitech.eu>
2016-11-20 00:19:25 +0100

    Minor changes to AtModule unit tests

--- tests/test_at_module.py
+++ tests/test_at_module.py
@@ -1,67 +1,73 @@
-import cnorm
 from src.at_module import AtModuleErrorMultiModule
 from src.at_module import AtModuleErrorMultiObj
 from src.at_module import AtModuleErrorNotInlineFonction
+
 from src.mangling import mangling
+from src.import_handler import ImportHandler
 from src.object_list import ObjectList
-from pyrser.error import Diagnostic
 from src import kooc_parser
 from src import at_module
 
+import cnorm
+from pyrser.error import Diagnostic
+
 import unittest
 
+ih = ImportHandler()
 par = cnorm.parsing.declaration.Declaration()
-kooc_parser = kooc_parser.KoocParser()
+kooc_parser = kooc_parser.KoocParser(ih)
 
 class TestAtModule(unittest.TestCase):
+    "Unit tests for AtModule"
 
-    def test_parsing_noargs(self):
-
-        # test sans arg
+    def test_parsing_empty(self):
+        "Test parsing empty module"
         parsed_module = kooc_parser.parse("@module foo {}").body[0]
         control_module = at_module.AtModule("foo", [])
         self.assertEqual(parsed_module, control_module)
 
-    def test_parsing_noname(self):
-
-        # test sans nom
+    def test_parsing_error(self):
+        "Test incorrect syntaxes"
         self.assertRaises(Diagnostic, kooc_parser.parse, "@module {}")
         self.assertRaises(Diagnostic, kooc_parser.parse, "@module")
         self.assertRaises(Diagnostic, kooc_parser.parse, '@module "bar" {}')
 
     def test_module_base(self):
-
-        # tests de base
-        parsed_module = kooc_parser.parse("@module foobar {int x;}")
-        control_module = at_module.AtModule("foobar", par.parse("int x;").body)
+        "Test parsing simple modules"
+        self.assertEqual(
+            kooc_parser.parse("@module foobar {int x;}"),
+            at_module.AtModule("foobar", par.parse("int x;").body)
+        )
+        parsed_module = kooc_parser.parse("""
+            @module foobar_one {
+                int x;
+                int x;
+            }
+        """)
+        control_module = at_module.AtModule(
+            "foobar_one",
+            par.parse("int x; int x;").body
+        )
         self.assertEqual(parsed_module, control_module)
 
-        parsed_module1 = kooc_parser.parse("""
-        @module foobar_one {int x;
-        int x;
-        }
-        """)
-        control_module1 = at_module.AtModule("foobar_one", par.parse("int x; int x;").body)
-        self.assertEqual(parsed_module1, control_module1)
+    def test_transform_empty(self):
+        "Test transforming empty module"
 
-    def test_transfo_noargs(self):
+        obj_list = ObjectList()
+        module = at_module.AtModule("foo", [])
+        self.assertEqual(module.get_c_ast(obj_list), [])
+
+    def test_transform_int(self):
+        "Test transforming module with single declaration"
+
         obj_list = ObjectList()
 
-        # test sans arg
-        control_module = at_module.AtModule("foo", [])
-        parsed_module = par.parse("")
-        self.assertEqual(control_module.get_c_ast(obj_list), parsed_module.body)
-
-    def test_transfo_int(self):
-        obj_list = ObjectList()
-
-        # test avec un int
         control_module = at_module.AtModule("bar", par.parse("int x;").body)
         parsed_module = par.parse("int x;").body[0]
         parsed_module._name = mangling(parsed_module, "bar")
         self.assertEqual(control_module.get_c_ast(obj_list), [parsed_module])
 
-    def test_transfo_moreints(self):
+    def test_transform_moreints(self):
         obj_list = ObjectList()
 
         # test avec plus d'ints
