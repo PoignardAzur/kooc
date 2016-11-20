0c1e31ef37c79b175bfe69b710145e3c3a421689
gabriel amice <amice_g@linux-jas9.suse>
2016-11-20 12:04:45 +0100

    Autre constructeur pour Exception

--- src/exception.py
+++ src/exception.py
@@ -3,19 +3,21 @@ from cnorm.passes import to_c
 from pyrser import error
 
 class ErrorClass():
-      def __init__(self, msg: str = None, filepath: str = None, col: str = None, line: str = None, diagnostic: error.Notification = None):
-            if diagnostic is not None:
-                  self.msg = diagnostic.get_content(True, True)
-                  self.filepath = diagnostic.location.filepath
-                  self.col = diagnostic.location.col
-                  self.line = diagnostic.location.line
-                  self.diagnostic = True
-            else :
+      def __init__(self, msg: str, filepath: str, col: str, line: str):
+
                   self.msg = msg
                   self.filepath = details
                   self.col = col
                   self.line = line
                   self.diagnostic = False
+
+      def __init__(self, diagnostic: error.Notification):
+
+                  self.msg = diagnostic.get_content(True, True)
+                  self.filepath = diagnostic.location.filepath
+                  self.col = diagnostic.location.col
+                  self.line = diagnostic.location.line
+                  self.diagnostic = True
 
 def my_get_content(self) -> str:
     f = open(self.filepath, 'r')
